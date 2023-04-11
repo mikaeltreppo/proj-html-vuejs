53473a86cc5c0e74281a9f26773caaed9fe3ca3d
mikaeltreppo <mikaeltreppo@outlook.com>
2023-04-11 17:51:11 +0200

    aggiornamenti main part courses senza vue

--- src/components/MyMain.vue
+++ src/components/MyMain.vue
@@ -1,7 +1,92 @@
 <template>
+    <div class="totalContainerCourses">
     <div class="mainContainer">
         <div class="mainCoursesPart">
-            <h1>main courses part</h1>
+            <div class="intro d-flex d-column a-c j-c">
+                <span>STIMULATED TO LEARN?</span>
+                <h2>Featured Online Courses</h2>
+            </div>
+            <div class="cardCourses d-flex wrap">
+                <!--course card-->
+                <div class="course d-flex">
+                    <div class="courseLeft">
+                        <img class="courseImg" src="../assets/course-02-480x298.jpg" alt="">
+                    </div>
+                    <div class="courseRight d-flex d-column a-c j-c">
+                        <h3 class="price green">£40</h3>
+                        <span>Learning to Write as a Professional
+                            Author</span>
+                        <span>20 lessons</span>
+                        <span>50 students</span>
+                    </div>
+                </div>
+
+                <div class="course d-flex">
+                    <div class="courseLeft">
+                        <img class="courseImg" src="../assets/course-02-480x298.jpg" alt="">
+                    </div>
+                    <div class="courseRight d-flex d-column a-c j-c">
+                        <h3 class="price green">£40</h3>
+                        <span>Learning to Write as a Professional
+                            Author</span>
+                        <span>20 lessons</span>
+                        <span>50 students</span>
+                    </div>
+                </div>
+
+                <div class="course d-flex">
+                    <div class="courseLeft">
+                        <img class="courseImg" src="../assets/course-02-480x298.jpg" alt="">
+                    </div>
+                    <div class="courseRight d-flex d-column a-c j-c">
+                        <h3 class="price green">£40</h3>
+                        <span>Learning to Write as a Professional
+                            Author</span>
+                        <span>20 lessons</span>
+                        <span>50 students</span>
+                    </div>
+                </div>
+
+                <div class="course d-flex">
+                    <div class="courseLeft">
+                        <img class="courseImg" src="../assets/course-02-480x298.jpg" alt="">
+                    </div>
+                    <div class="courseRight d-flex d-column a-c j-c">
+                        <h3 class="price green">£40</h3>
+                        <span>Learning to Write as a Professional
+                            Author</span>
+                        <span>20 lessons</span>
+                        <span>50 students</span>
+                    </div>
+                </div>
+
+                <div class="course d-flex">
+                    <div class="courseLeft">
+                        <img class="courseImg" src="../assets/course-02-480x298.jpg" alt="">
+                    </div>
+                    <div class="courseRight d-flex d-column a-c j-c">
+                        <h3 class="price green">£40</h3>
+                        <span>Learning to Write as a Professional
+                            Author</span>
+                        <span>20 lessons</span>
+                        <span>50 students</span>
+                    </div>
+                </div>
+
+                <div class="course d-flex">
+                    <div class="courseLeft">
+                        <img class="courseImg" src="../assets/course-02-480x298.jpg" alt="">
+                    </div>
+                    <div class="courseRight d-flex d-column a-c j-c">
+                        <h3 class="price green">£40</h3>
+                        <span>Learning to Write as a Professional
+                            Author</span>
+                        <span>20 lessons</span>
+                        <span>50 students</span>
+                    </div>
+                </div>
+            </div>
+        </div>
         </div>
         <div class="mainTutoringPart">
             <h1>main tutoring part</h1>
@@ -18,13 +103,40 @@
     </div>
 </template>
 
-<script></script>
+<script>
 
-<style>
-.mainCoursesPart {
-    height: 450px;
-    background-color: burlywood;
+</script>
+
+<style scoped>
+.mainContainer{
+    padding: 30px 0;
 }
+.totalContainerCourses{
+    background-color: var(--bgDeepGray);
+}
+/*setup card*/
+
+.course{
+    width: calc(100% / 2);
+    padding: 15px;
+}
+.courseImg {
+    display: inline;
+    margin: 0 auto;
+    margin-left: -25%;
+    height: 100%;
+    width: auto;
+}
+
+.courseLeft {
+    width: 150px;
+    height: 150px;
+    position: relative;
+    overflow: hidden;
+    border-radius: 50%;
+}
+
+
 
 .mainTutoringPart {
     height: 450px;
